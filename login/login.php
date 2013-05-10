78deedad9544c5c60a0db813569035402b900781
Marin Salinas <m.mslns27@gmail.com>
2013-05-09 23:13:58 -0500

    login de Clientes o Admin

--- login/login.php
+++ login/login.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>MaxaHarris - Login</title>
 </head>
 <body>
 <center>
 	<h1>Ingresa tu usuario y contrase&ntilde;a</h1>
 	<table>
 		<form name='login' action='loginvalidate.php' method='post'/>
+		<tr><td>Tipo de Usuario:</td><td><select name='tipoLogin'><option value='1'>Cliente</option><option value='0'>Administrador</option></select></td></tr>
 		<tr><td>Usuario:</td><td><input type='text' name='user'></td></tr>
 		<tr><td>Contraseña:</td><td><input type='password' name='pass'></td></tr>
 		<tr><td><input type='submit' value='Entrar' /></td></tr>
--- login/loginvalidate.php
+++ login/loginvalidate.php
@@ -4,10 +4,22 @@ require_once("../config.php");
 
 $user=$_POST['user'];
 $pass=$_POST['pass'];
+$priv=$_POST['tipoLogin'];
 
-function valid($usr, $pas){
+function validUser($usr, $pas){
 	$sql= "SELECT idclientes, clientesNombre, clientesUsername, clientesPassword 
 	FROM clientes WHERE clientesUsername ='$usr' AND clientesPassword = '$pas' LIMIT 1";
+	//echo "<br>".$sql."</br>";
+	$result=mysql_query($sql);
+	while($row=mysql_fetch_object($result)) {
+		$usuario = $row;
+	}
+	return $usuario;
+}
+
+function validAdmin($usr, $pas){
+	$sql= "SELECT idadministrador, administradorNombre, administradorUsername, administradorPassword, administradorPrivilegios  
+	FROM administrador WHERE administradorUsername ='$usr' AND administradorPassword = '$pas' LIMIT 1";
 	//echo "<br>".$sql."</br>";
 	$result=mysql_query($sql);
 	while($row=mysql_fetch_object($result)) {
@@ -25,11 +37,16 @@ if(empty($user)||empty($pass)){
 $pass=md5($pass);
 //echo $pass;
 
-$usuario=valid($user, $pass);
+if($priv == 1){
+$usuario=validUser($user, $pass);
+}
+if ($priv == 0) {
+	$usuario=validAdmin($user, $pass);
+}
+
 //echo "<pre>";
 //print_r($usuario);
 //echo "</pre>";
-
 
 if(!empty($usuario)){
 $_SESSION['usuario'] = $usuario;
