12b1550164b712a8876865e38656e9ddba5de8a9
Marin Salinas <m.mslns27@gmail.com>
2013-05-09 23:15:24 -0500

    Index para detectar tipoUsr

--- main/Index.php
+++ main/Index.php
@@ -10,7 +10,20 @@
 	require_once("../login/valid.php");
 	$usuario = $_SESSION['usuario'];
 	//print_r($usuario);
-	echo"<p>"."Bienvenido ".$usuario->clientesNombre;
+	//Con este for each obtenemos el valor del privilegio aunque podemos hacer una arreglo
+	//y sería el ultimo indice, pero en este caso no lo ocupamos
+	foreach ($usuario as $key => $us) {
+		$privilegio = $us;
+	}
+	//echo "<br>Privilegio:".$privilegio;
+	//si privilegio == 1;
+	if($privilegio == 1){
+		echo"<p>"."Bienvenido ".$usuario->clientesNombre;
+	}
+	if($privilegio == 0){
+		echo"<p>"."Bienvenido ".$usuario->administradorNombre;
+	}
+
 	?>
 	<a href="../login/logout.php">Logout</a></p>
 </body>
