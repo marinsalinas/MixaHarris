2ebf4499d47f927f4c97efeeb793edd25110b06c
Alberto Cortes Blanco <c.n.albertocortes@gmail.com>
2013-05-17 19:08:22 -0500

    indexAdmin

--- main/pagindex/adminindex.php
+++ main/pagindex/adminindex.php
@@ -1,9 +1,15 @@
 <!doctype html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Bienvenido Adminstrador</title>
+		<meta charset="UTF-8">
+		<link rel='stylesheet' type='text/css' href = "../../lib/styleAdmin.css">
+		<link href='http://fonts.googleapis.com/css?family=Mouse+Memoirs' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="lib/jquery.js"></script>
+		<script type="text/javascript" src="lib/lib.js"></script>
+		<title>MixaHarris Adminstrador</title>
 </head>
+
+
 <body>
 	<?php
 	session_start();
@@ -11,11 +17,55 @@
 	require_once("../../login/valid.php");
 	require_once("../../login/validpriv.php");
 	validAdmin();
-	echo "<h1>MixaHarris - Admin Index Nuevo </h1>";
+	//echo "<h1>MixaHarris - Admin Index Nuevo </h1>";
 	$admin = $_SESSION['usuario'];
 	//print_r($usuario);
-		echo"<p>"."Bienvenido ".$admin->administradorNombre;
+	//echo "<div id='navegador'>";
+		//echo"<p>"."Bienvenido ".$admin->administradorNombre."<a href='../../login/logout.php'>Logout</a></p>";
+	//echo "</div>";
 	?>
-	<a href="../../login/logout.php">Logout</a></p>
+	<!--<a href="../../login/logout.php">Logout</a></p>-->
+
+		<div id='navegador'>
+			<div id='homeLogo'>MixaHarris Administrador</div>
+				<div id='navAdmin'>	
+					Hola, <?php echo $admin->administradorNombre;?>
+					<form action='../../login/logout.php' id='blogout'><input id='button' type='submit' value='Logout'></form>
+				 </div>
+		</div><!--navegador-->
+		
+		
+		
+		<!--Cuerpo de pagina-->
+		<div id='containerAdmin'>
+				
+				<div id='sidebarAdmin'>
+					<ul>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+						<li>kdbclkasdjbvlkdas</li>
+					
+					</ul
+				
+				
+				</div>
+				
+				<div id='menuAdmin'>
+					sadgadsfasfadsfadsfasdfragñbjvalergfnlakhnclwabnegvlua.kjasnflkj
+				</div>
+		
+		
+		
+		
+		
+		
+		</div>
+		
 </body>
 </html>
+
