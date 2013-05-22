79fa76545fd0aafd2c761525b0c6e07c3c966a4f
Alberto Cortes Blanco <c.n.albertocortes@gmail.com>
2013-05-21 23:11:43 -0500

    sobres

--- login/logout.php
+++ login/logout.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
 session_destroy();
-<<<<<<< HEAD
-=======
 //header('Location: login.php');
->>>>>>> 7459c9347d291852bb00f7a034aabaee8628ac4a
 header('Location: ../index.html');
 ?>
