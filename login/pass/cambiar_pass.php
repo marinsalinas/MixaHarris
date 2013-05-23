b973b335bc1ee24bad8f44fea146484bc2970d1f
Marin Salinas <m.mslns27@gmail.com>
2013-05-22 22:05:34 -0500

    sobres

--- login/pass/cambiar_pass.php
+++ login/pass/cambiar_pass.php
@@ -18,11 +18,35 @@ session_start();
 		$priv = $atrib;
 		break;
 	}
-
-	//echo "<br>";
-	//print_r($privilegio);
+	//es para poner en nombre en la pagina
+	$nombre =(($priv == 1) ? $user->clientesNombre : $user->administradorNombre);
 ?>
 
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name='description' content=''/>
+	<meta http-equiv='X-UA-Compatible' content='IE=edge, chrome=1'/>
+	<link rel='stylesheet' type='text/css' href = "../../lib/styleUser.css">
+	<link href='http://fonts.googleapis.com/css?family=Mouse+Memoirs' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="../../lib/jquery.js"></script>
+	<script type="text/javascript" src="../../lib/lib.js"></script>
+	<title>Cambiar Contraseña</title>
+</head>
+<body>
+	<div id='navegador'>
+			<div id='homeLogo' onclick="location.href='../index.html'">MixaHarris Home</div>
+			<div id='login'>	
+	
+			</div>
+		</div>
+	
+
+<center><div id='menuAltaClientes'>
+						</br></br>
+							<p class='hdm'>Cambiar Contraseña para <?php echo $nombre; ?></p></br></br>
+</br></br>
 <table id='changePassword'>
 	<form name='altasProveedor' action="validpass.php" method='post' accept-charset='utf-8'>
 		<input type='hidden' name='id' value=<?php echo "'".$id."'" ?>/>
@@ -31,4 +55,13 @@ session_start();
 		<tr><td>Nueva Contraseña:</td><td><input type='password' name='nuevo'/></td></tr>
 		<tr><td colspan='2'></br><center><input id='button' type='submit'value='Aceptar'/></center></td></tr>
 	</form>
-</table>
+</table>	
+</br></br>
+</div></center><!--Div de menu-->
+
+</body>
+</html>
+
+
+
+
