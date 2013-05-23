65a73496e61d7a9b37d5c187c73ab002063a6de7
Marin Salinas <m.mslns27@gmail.com>
2013-05-23 07:44:24 -0500

    lksdgfksjhdkjgkadskjasaghjfksdhgdhjagskjhghjfk

--- formulario/action/save_clientes.php
+++ formulario/action/save_clientes.php
@@ -13,29 +13,43 @@
 <title>MixaHarris - Se dio de alta</title>
 <body>
 		<?php
-	//session_start();
+	session_start();
 	//lo mismo
 	//require_once("../../login/valid.php");
 	//require_once("../../login/validpriv.php");
 	//validAdmin();
 	//echo "<h1>MixaHarris - Admin Index Nuevo </h1>";
+if (empty($_SESSION['usuario'])){
+	echo "<div id='navegador'>
+			<div id='homeLogo'>MixaHarris Home</div>
+			<div id='login'>	
+				<form id='lin' name='LogIN' action='login/loginvalidate.php' method='post' accept-charset='utf-8'>
+				Entrar como: <select name='tipoLogin' id='tipoLogin'><option value='1'>Cliente</option><option value='0'>Admin</option></select>
+				Usuario: <input class='form' type='text' name='user' id='user'>
+				Contraseña: <input class='form' type='password' name='pass' id='pass'>
+				 <input id='button' type='button' value='Aceptar' onclick='login();'></form>
+			</div>
+		</div>";
+}else{
+	
 	$admin = $_SESSION['usuario'];
 	//print_r($usuario);
 	//echo "<div id='navegador'>";
 		//echo"<p>"."Bienvenido ".$admin->administradorNombre."<a href='../../login/logout.php'>Logout</a></p>";
 	//echo "</div>";
-	?>
-	<div id='navegador'>
+	
+	echo "<div id='navegador'>
 			<div id='homeLogo'>MixaHarris Administrador - Alta Clientes</div>
 				<div id='navAdmin'>	
-					Hola, <?php echo $admin->administradorNombre;?> 
+					Hola, {$admin->administradorNombre}?> 
 					<form action='../../login/logout.php' id='blogout'>
 						<input id='button' type='submit' value='Logout'>	
 					</form> 
-					<a class="Bca" href="../../main/pagindex/adminindex.php" title="Menú administrador">conf</a> 
+					<a class='Bca' href='../../main/pagindex/adminindex.php' title='Menú administrador'>conf</a> 
 				 </div>
-		</div><!--navegador-->
-
+		</div><!--navegador-->";
+	}
+	?>
 
 	<center><div id="menuSaveClientes">
 		<center><h2>STATUS</h2></center>
